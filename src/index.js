7c4bbfaa0f0867b5233197f178810ffb043c5fc9
Your Name <pavlo.morozenko@ukr.net>
2023-03-03 20:09:43 +0100

    3

--- src/index.js
+++ src/index.js
@@ -13,4 +13,29 @@ let searchCountryName = '';
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
-function onInputChange() {}
+function onInputChange() {
+  searchCountryName = input.value.trim();
+  if (searchCountryName === '') {
+    clearAll();
+    return;
+  } else
+    fetchCountries(searchCountryName)
+      .then(countryNames => {
+        if (countryNames.length < 2) {
+          createCoutryCard(countryNames);
+          Notiflix.Notify.success('This is your result');
+        } else if (countryNames.length < 10 && countryNames.length > 1) {
+          createCoutryList(countryNames);
+          Notiflix.Notify.success('This is your results');
+        } else {
+          clearAll();
+          Notiflix.Notify.info(
+            'Too many matches found, please enter more specific name.'
+          );
+        }
+      })
+      .catch(() => {
+        clearAll();
+        Notiflix.Notify.failure('Ooops, there is no country with that name.');
+      });
+}
