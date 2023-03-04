9a6782f9401a286a21519ed52b39845901be0aa4
Your Name <pavlo.morozenko@ukr.net>
2023-03-04 15:05:32 +0100

    7

--- src/index.js
+++ src/index.js
@@ -66,12 +66,18 @@ function createCountryCard(country) {
 function createCountryList(country) {
   clearAll();
   const readyList = country
-    .map((
-      c => `<li class="country-list--item">
+    .map(
+      c =>
+        `<li class="country-list--item">
             <img src="${c.flags.svg}" alt="Country flag" width="40", height="30">
             <span class="country-list--name">${c.name.official}</span>
         </li>`
     )
     .join('');
-  list.insertAdjacentElement('beforeend, readyList');
+  list.insertAdjacentHTML('beforeend', readyList);
+}
+
+function clearAll() {
+  list.innerHTML = '';
+  div.innerHTML = '';
 }
